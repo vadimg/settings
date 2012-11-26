36dc3c3366000b14733d54df5cdc1522ce94912f
Vadim Graboys <dimva13@gmail.com>
2012-11-25 23:11:04 -0500

    make install.py actually install stuff (oops)

--- install.py
+++ install.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import sys
 
 from copyfrom import get_files
 
@@ -9,3 +10,7 @@ def main():
     for filename in get_files():
         print 'Copying', filename
         shutil.copy2(filename, os.path.join(os.path.expanduser('~'), filename))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
