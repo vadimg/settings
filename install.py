e52002c6074abc2623afc06e9015805ce95a4dca
Vadim Graboys <vadim@chartbeat.com>
2012-12-19 15:14:20 -0500

    mkdir_p in install.py

--- install.py
+++ install.py
@@ -3,13 +3,26 @@
 import os
 import shutil
 import sys
+import errno
 
 from copyfrom import get_files
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
 def main():
     for filename in get_files():
         print 'Copying', filename
-        shutil.copy2(filename, os.path.join(os.path.expanduser('~'), filename))
+        new_filename = os.path.join(os.path.expanduser('~'), filename)
+        mkdir_p(os.path.dirname(new_filename))
+        shutil.copy2(filename, new_filename)
 
 
 if __name__ == '__main__':
